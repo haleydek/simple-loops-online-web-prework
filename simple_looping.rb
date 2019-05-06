cc9bc462b65d30c5b2e16f991da9bf43129f3b42
Haley DeKeyser <hcd09@live.com>
2019-05-06 01:49:50 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,6 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator(number_of_times)
+def loop_iterator(7)
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
   loop do
     counter += 1
     puts phrase
-    break if counter > number_of_times
+    break if counter > 7
   end
 end
 
